4dce628614e8e03e750c56c7a9bcd8a87939a6fd
Jared <brasschaos@gmail.com>
2014-05-07 13:12:04 -0700

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,13 +1,38 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
+
+// Rescue Mission:
+// this.moveDown();
+// this.moveDown();
+// this.moveRight();
+// this.moveUp();        Wait for it
+// this.moveUp();
+// this.moveRight();
+// this.moveRight();
+// this.moveRight();      Wait for iiiiit
+// this.moveDown();
+// this.attackNearbyEnemy();   CLOBBER
+
+
+// Grab The Mushroom:
+
+// Drink Me:
+
+// Taunt the Guards:
+
+// It's a Trap!
  
+// Break the Prison:
+
+// if (name === "William" || name === "Lucas" || name === "Marcus" || name === "Robert" || name ==="Gordon")
+//     return true;
  
- 
- 
- 
+Taunt:
+
+Cowardly Taunt:
  
  
  
