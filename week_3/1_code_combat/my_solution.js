d24a6a0c4c75cf9c3514a58a7dfe4e423d5e08f0
Jared <brasschaos@gmail.com>
2014-05-07 17:02:50 -0700

    added my reflection

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -4,38 +4,97 @@
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
 
-// Rescue Mission:
-// this.moveDown();
-// this.moveDown();
-// this.moveRight();
-// this.moveUp();        Wait for it
-// this.moveUp();
-// this.moveRight();
-// this.moveRight();
-// this.moveRight();      Wait for iiiiit
-// this.moveDown();
-// this.attackNearbyEnemy();   CLOBBER
+"Rescue Mission:"
+// pretty straightforward
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy(); //KILL
 
 
-// Grab The Mushroom:
+"Grab The Mushroom:"
 
-// Drink Me:
+this.moveUp();
+this.moveRight(); //POWER UP
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
 
-// Taunt the Guards:
+"Drink Me:"
 
-// It's a Trap!
+this.moveRight();
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();     //HEALZ
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy(); 
+
+"Taunt the Guards:"
+this.moveRight();
+this.bustDownDoor();
+this.moveRight();
+this.say("Hey noob");   //lols
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+this.say("lol");    //unnecessary but lols
+this.moveRight();
+this.say("Follow me.");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+this.say("OI!");
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveRight();
+
+
+"It's a Trap!"
+// lol ^^^^
+this.moveDown();
+this.moveDown();
+this.say("hey noob");
+this.moveUp();
+this.say("I'll pwn u");
+this.moveUp();
+this.say("HAHA");
+this.moveDown();
+this.say("OW");    //had to mess around
+
  
-// Break the Prison:
+"Break the Prison:"
+//simple right?
+if (name === "William" || name === "Lucas" || name === "Marcus" || name === "Robert" || name ==="Gordon")
+     return true;
+ 
+"Taunt:"
+//lols
+this.say("ROGURE LOLOLOLOL@!!111eleven");
+this.say("ILL PWN U NUB");
+this.say("OMG LOL");
+this.say("HAHA");
 
-// if (name === "William" || name === "Lucas" || name === "Marcus" || name === "Robert" || name ==="Gordon")
-//     return true;
- 
-Taunt:
 
-Cowardly Taunt:
- 
- 
+"Cowardly Taunt:"
+//more lols, plus i cut it short like a boss
+ this.moveXY(50, 16);
+this.say("ya'll ogres need jesus");
+this.moveXY(63, 20);
+this.say("oh noes might die");
  
  
 // Reflection:
 // Write your reflection here.
+"Hilarious! Very cleverly made. Voices a bit much but good music. it was totally fun and i totally enjoyed messing with it."
+"screenshot of brasschaos(me) on pvp leaderboard coming soon, ive already played like 4 matches"
+
+
